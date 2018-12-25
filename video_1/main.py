69062bf551286b5cccf5f21d32ca565ce9457883
Sergey-Volchkov <seriyvlastelin@gmail.com>
2018-12-25 20:36:09 +0300

    Commit cod from from the first lesson

---
+++ video_1/main.py
@@ -0,0 +1,19 @@
+from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api
+from datetime import datetime
+
+login, password = "login", "password"
+vk_session = vk_api.VkApi(login, password)
+vk_session.auth()
+
+# token = "ЗдесьЕстьВашТокен"
+# vk_session = vk_api.VkApi(token=token)
+
+session_api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
+
+while True:
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Сообщение пришло в: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
+            print('Текст сообщения: ' + str(event.text))
